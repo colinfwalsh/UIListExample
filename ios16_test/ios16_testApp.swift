d8c912e152d7c77084dcff3b246e7f89d75886be
Colin Walsh <colinwalsh@gmail.com>
2022-06-07 15:44:37 -0400

    Initial Commit

---
+++ ios16_test/ios16_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios16_testApp.swift
+//  ios16_test
+//
+//  Created by Colin Walsh on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ios16_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
